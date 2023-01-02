8c8f659a18356b6339b0bdef6c57058be7c02744
sinhyeonho <gusgh4922@gmail.com>
2023-01-02 14:45:36 +0900

    memoApp start

---
+++ MemoApp/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  MemoApp
+//
+//  Created by HyeonHo on 2023/01/02.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
