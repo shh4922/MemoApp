c8f79289560a55ef264c648f84c07b7974f90382
sinhyeonho <gusgh4922@gmail.com>
2023-01-02 14:44:23 +0900

    Initial Commit

---
+++ MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by HyeonHo on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
